5193aa4ac972f133980aa803a8144ae38d293e50
Vadim Shurman <ShurmanV@gmail.com>
2018-02-01 13:21:42 +0200

    add test log in route

--- app/main/views/bot.py
+++ app/main/views/bot.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app.main.forms import BotForm
 from .. import main
+from manage import app
 
 
 @main.route('/bot/frame', methods=['GET', 'POST'])
@@ -16,4 +17,6 @@ def bot_api():
     form = BotForm()
     # resp = make_response(redirect(url_for('.index')))
     # return resp
+    app.logger.info("route bot_api is selected.")
+    # todo check input and logging
     return render_template('bot.html', form=form)
