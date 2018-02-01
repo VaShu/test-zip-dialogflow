0f82b8461381c64d8460d5390cd3fe38be5f3fe7
Vadim Shurman <ShurmanV@gmail.com>
2018-02-01 13:20:15 +0200

    add logging

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
 from config import config
 
 bootstrap = Bootstrap()
@@ -15,6 +18,8 @@ def create_app(config_name):
 
     bootstrap.init_app(app)
     db.init_app(app)
+    enable_file_logging(app)
+    app.logger.info("Logging is set up.")
 
     # import blueprint
     from .main import main as main_blueprint
@@ -25,3 +30,30 @@ def create_app(config_name):
     app.register_blueprint(api_v1_blueprint, url_prefix='/api/v1')
 
     return app
+
+def enable_file_logging(app):
+
+    logging_path = app.config['LOGGING_PATH']
+
+    # The maxBytes is set to this number, in order to demonstrate the generation of multiple log files (backupCount).
+    # handler = RotatingFileHandler('logs/app.log', maxBytes=10000, backupCount=3)
+
+    """
+    https://docs.python.org/3.5/library/logging.handlers.html
+    You can use the when to specify the type of interval. The list of possible values is below. Note that they are not case sensitive.
+    
+    'S' 	    Seconds
+    'M' 	    Minutes
+    'H' 	    Hours
+    'D'     	Days
+    'W0'-'W6' 	Weekday (0=Monday)
+    'midnight' 	Roll over at midnight
+    """
+    handler = TimedRotatingFileHandler(logging_path, when='H', interval=1, backupCount=0)
+
+    # getLogger('__name__') - decorators loggers to file / werkzeug loggers to stdout
+    # getLogger('werkzeug') - werkzeug loggers to file / nothing to stdout
+    logging.getLogger('__name__')
+    handler.setLevel(logging.INFO)
+    app.logger.addHandler(handler)
+
