b450dc00cebe5477906f2260954221d1408cee12
Vadim Shurman <ShurmanV@gmail.com>
2018-02-08 06:36:35 +0200

    rem

--- app/api/v1/bot.py
+++ app/api/v1/bot.py
@@ -5,10 +5,9 @@ import dialogflow
 
 # dialogflow.ai account info
 CLIENT_ACCESS_TOKEN = "40371e6bed654b3daa71e2c6de958326"
-ai = dialogflow.AgentsClient()
+# ai = dialogflow.AgentsClient()
 
-
-@api.route('/*bot/<string:text_quest>')
+@api.route('/*bo t/<string:text_quest>')
 def get_answer(text_quest):
     if text_quest is not None:
         msg = {"question": text_quest}
