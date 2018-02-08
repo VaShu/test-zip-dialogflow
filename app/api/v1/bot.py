3a69c800a8f8203e3736eec5ad8e02a849ec3628
Vadim Shurman <ShurmanV@gmail.com>
2018-02-08 06:25:16 +0200

    api for bot

---
+++ app/api/v1/bot.py
@@ -0,0 +1,21 @@
+from flask import jsonify
+from . import api
+from manage import app
+import dialogflow
+
+# dialogflow.ai account info
+CLIENT_ACCESS_TOKEN = "40371e6bed654b3daa71e2c6de958326"
+ai = dialogflow.AgentsClient()
+
+
+@api.route('/*bot/<string:text_quest>')
+def get_answer(text_quest):
+    if text_quest is not None:
+        msg = {"question": text_quest}
+        print("from bot - qw:", text_quest)
+        app.logger.info("from bot - qw:")
+        app.logger.info(text_quest)
+        return jsonify(msg)
+    else:
+        msg = {"msg": "not question - not answer"}
+        return jsonify(msg)
